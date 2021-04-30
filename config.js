4bbda053888009b9225105dabdd6d0069d8c459a
Crispy-media <82466118+Crispy-media@users.noreply.github.com>
2021-04-30 09:25:54 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+{
+  "invite": "true",
+  "prefix": "%",
+  "discord_owner_id": "Your-Discord-ID",
+  "token": "Your-Bot-Token",
+  "youtubeAPI": "youtube-api-key",
+  "geniusLyricsAPI": "genius-api-key",
+  "tenorAPI": "tenor-API-key",
+  "newsAPI": "news-api-key",
+  "twitchClientID": "Your-Client-ID",
+  "twitchClientSecret": "Your-Client-Secret"
+}
